e637dc8944287edd1095587436eb60b5e6008330
Chen Ning <chenningg97@gmail.com>
2020-08-23 00:47:12 +0800

    Edited header partial and styling

--- themes/haru/tailwind.config.js
+++ themes/haru/tailwind.config.js
@@ -9,6 +9,14 @@ module.exports = {
     ],
   },
   theme: {
+    container: {
+      screens: {
+        sm: "100%",
+        md: "100%",
+        lg: "900px",
+        xl: "900px",
+      },
+    },
     extend: {
       height: (theme) => ({
         "screen-1/2": "50vh",
@@ -17,6 +25,14 @@ module.exports = {
         "screen-1/5": "calc(100vh / 5)",
         "screen-2/3": "calc(100vh * (2/3))",
         "screen-3/4": "75vh",
+      }),
+      width: (theme) => ({
+        "screen-1/2": "50vw",
+        "screen-1/3": "calc(100vw / 3)",
+        "screen-1/4": "25vw",
+        "screen-1/5": "calc(100vw / 5)",
+        "screen-2/3": "calc(100vw * (2/3))",
+        "screen-3/4": "75vw",
       }),
     },
   },

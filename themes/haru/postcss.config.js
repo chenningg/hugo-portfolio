1494910fa139bcf45577a881c39ef5deeb04175a
Chen Ning <chenningg97@gmail.com>
2020-08-24 14:54:07 +0800

    Added footer and updated partials

--- themes/haru/postcss.config.js
+++ themes/haru/postcss.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   plugins: [
-    require("postcss-import")({
-      path: ["themes/haru/assets/css"],
-    }),
+    require("postcss-import")({ path: "themes/haru/assets/css" }),
     require("tailwindcss")("./themes/haru/tailwind.config.js"),
     require("autoprefixer"),
   ],

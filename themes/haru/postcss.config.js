909baaec71254f7a6e4b3729cd4622ed1e84431f
Chen Ning <chenningg97@gmail.com>
2020-08-27 02:01:03 +0800

    Set HUGO_ENV instead of NODE_ENV

--- themes/haru/postcss.config.js
+++ themes/haru/postcss.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   plugins: [
-    require("postcss-import")({ path: "themes/haru/assets/css" }),
+    require("postcss-import")({
+      path: "themes/haru/assets/css",
+    }),
     require("tailwindcss")("./themes/haru/tailwind.config.js"),
     require("autoprefixer"),
   ],

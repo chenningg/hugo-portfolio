5c384c85510bc4cdb8db0deea83a820e9b4f29d8
Chen Ning <chenningg97@gmail.com>
2020-09-09 18:25:47 +0800

    Added prism syntax highlighting and updated footer

--- themes/haru/tailwind.config.js
+++ themes/haru/tailwind.config.js
@@ -21,22 +21,6 @@ module.exports = {
       },
     },
     extend: {
-      height: {
-        "screen-1/2": "50vh",
-        "screen-1/3": "calc(100vh / 3)",
-        "screen-1/4": "25vh",
-        "screen-1/5": "calc(100vh / 5)",
-        "screen-2/3": "calc(100vh * (2/3))",
-        "screen-3/4": "75vh",
-      },
-      width: {
-        "screen-1/2": "50vw",
-        "screen-1/3": "calc(100vw / 3)",
-        "screen-1/4": "25vw",
-        "screen-1/5": "calc(100vw / 5)",
-        "screen-2/3": "calc(100vw * (2/3))",
-        "screen-3/4": "75vw",
-      },
       colors: {
         twitter: "#1da1f2",
       },
